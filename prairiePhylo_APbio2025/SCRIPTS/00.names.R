be8a9dd71e19077f2a101c2fbda38d892bb2e952
Andrew Hipp <AHipp@mortonarb.org>
2025-09-30 21:05:49 -0500

    Updating files for APbio2025 prairie

--- prairiePhylo_APbio2025/SCRIPTS/00.names.R
+++ prairiePhylo_APbio2025/SCRIPTS/00.names.R
@@ -3,12 +3,12 @@ library(WorldFlora)
 if(!exists('WFO.data')) {
   tempWFO = try(WFO.remember('classification.txt'))
   if(class(tempWFO) == 'try-error') {
-    WFO.download(method = "wget", extra = "--no-check-certificate") # added 2025-02-28
+    WFO.download(extra = "--no-check-certificate") # added 2025-02-28
   }
   rm(tempWFO)
 }
 
-dat.names <- dat.names.orig <- read.csv('prairiePhylo_2.2/DATA/dat.names.barber25.csv')
+dat.names <- dat.names.orig <- read.csv('DATA/dat.names_APbio25.csv')
 dat.wfoMatch <-
   WFO.match(gsub('_', ' ', dat.names$treeName, fixed = T),
             WFO.data = WFO.data,
@@ -17,6 +17,6 @@ dat.wfoOne <- WFO.one(dat.wfoMatch)
 dat.names$accepted <- dat.wfoOne$scientificName
 dat.names$author <- dat.wfoOne$scientificNameAuthorship
 
-write.csv(dat.wfoMatch, 'prairiePhylo_2.2/OUT/dat.wfoMatch.csv')
-write.csv(dat.wfoOne, 'prairiePhylo_2.2/OUT/dat.wfoOne.csv')
-write.csv(dat.names, 'prairiePhylo_2.2/OUT/dat.names.updated.csv')
+write.csv(dat.wfoMatch, 'OUT/dat.wfoMatch.csv')
+write.csv(dat.wfoOne, 'OUT/dat.wfoOne.csv')
+write.csv(dat.names, 'OUT/dat.names.updated.csv')
--- prairiePhylo_APbio2025/SCRIPTS/01.tree.R
+++ prairiePhylo_APbio2025/SCRIPTS/01.tree.R
@@ -11,22 +11,19 @@ if(!exists('dat.tr')) {
   temp <- tempfile()
   download.file(smithBrownTree,temp)
   dat.tr <- read.tree(unz(temp, "v0.1/ALLMB.tre"))
-  write.tree(dat.tr, 'prairiePhylo_2.2/DATA/ALLMB.tre') # added 2025-02-28 as fail-safe
+  write.tree(dat.tr, 'DATA/ALLMB.tre') # added 2025-02-28 as fail-safe
   unlink(temp)
 } # close if
 
 
 dat.tr2 <- dat.tr
-dat.tr2 <-
-  drop.tip(dat.tr2, setdiff(dat.tr2$tip.label, dat.names$splicedTreeName))
-dat.tr2 <- force.ultrametric(dat.tr2)
 
 ## weld on singletons
 
 ## added 2025-02-28 to get singletons back in more generally
 ## assumes the genus is present and represented by > 1 sp
 ## welds tip to base of genus
-tipsToAdd <- setdiff(dat.names$splicedTreeName, dat.tr2$tip.label)
+tipsToAdd <- setdiff(dat.names$treeName, dat.tr2$tip.label)
 
 if(length(tipsToAdd) > 0) {
   nodes <- sapply(tipsToAdd, function(x) {
@@ -49,49 +46,54 @@ for(i in names(nodes)) {
   rm(nodeTemp)
 } # close for
 
+dat.tr2 <-
+  drop.tip(dat.tr2, setdiff(dat.tr2$tip.label, dat.names$treeName))
+dat.tr2 <- force.ultrametric(dat.tr2)
+
+
 ## Fix Carex
-dat.carex <- read.tree('https://raw.githubusercontent.com/andrew-hipp/2019-GCG/master/DATA/Carex10Fossil.divTree.tre')
-dat.carex2 <-
-  drop.tip(dat.carex,
-            grep('Carex', dat.names$treeName, value = T) %>%
-            paste(collapse = '|') %>%
-            grep(x = dat.carex$tip.label, invert = T, value = T)
-          ) # close drop.tip
-dat.carex2$tip.label <-
-  label.elements(dat.carex2, returnNum=1, fixed = T) %>% as.character
+# dat.carex <- read.tree('https://raw.githubusercontent.com/andrew-hipp/2019-GCG/master/DATA/Carex10Fossil.divTree.tre')
+# dat.carex2 <-
+#   drop.tip(dat.carex,
+#             grep('Carex', dat.names$treeName, value = T) %>%
+#             paste(collapse = '|') %>%
+#             grep(x = dat.carex$tip.label, invert = T, value = T)
+#           ) # close drop.tip
+# dat.carex2$tip.label <-
+#   label.elements(dat.carex2, returnNum=1, fixed = T) %>% as.character
 
-dat.tr3 <-
-  drop.tip(dat.tr2,
-            grep('Carex', dat.tr2$tip.label, value = T),
-            trim.internal = F
-          )
+# dat.tr3 <-
+#   drop.tip(dat.tr2,
+#             grep('Carex', dat.tr2$tip.label, value = T),
+#             trim.internal = F
+#           )
 
-dat.subtreeDepth <-
-  max(node.depth.edgelength(dat.tr3)) -
-  node.depth.edgelength(dat.tr3)[which(dat.tr3$tip.label == '')]
+# dat.subtreeDepth <-
+#   max(node.depth.edgelength(dat.tr3)) -
+#   node.depth.edgelength(dat.tr3)[which(dat.tr3$tip.label == '')]
 
-carex.pos <- max(dat.carex2 %>% node.depth.edgelength) - dat.subtreeDepth
+# carex.pos <- max(dat.carex2 %>% node.depth.edgelength) - dat.subtreeDepth
 
-dat.tr3 <-
-  bind.tree(dat.tr3, dat.carex2,
-            where = which(dat.tr3$tip.label == ''),
-            position = carex.pos)
-dat.tr3 <- drop.tip(dat.tr3, '')
+# dat.tr3 <-
+#   bind.tree(dat.tr3, dat.carex2,
+#             where = which(dat.tr3$tip.label == ''),
+#             position = carex.pos)
+# dat.tr3 <- drop.tip(dat.tr3, '')
 
 ## write trees
 dat.tr.analysis <- dat.tr.figures <- ladderize(dat.tr3)
 dat.tr.analysis$tip.label <-
-  dat.names$phylo.spp[match(dat.tr3$tip.label, dat.names$splicedTreeName)]
+  dat.names$phylo.spp[match(dat.tr3$tip.label, dat.names$treeName)]
 dat.tr.figures$tip.label <-
-  dat.names$accepted[match(dat.tr3$tip.label, dat.names$splicedTreeName)]
+  dat.names$accepted[match(dat.tr3$tip.label, dat.names$treeName)]
 
-write.tree(dat.tr.analysis, 'prairiePhylo_2.2/OUT/tr.analysis.tre')
-write.tree(dat.tr.figures, 'prairiePhylo_2.2/OUT/tr.acceptedNames.tre')
+write.tree(dat.tr.analysis, 'OUT/tr.analysis.tre')
+write.tree(dat.tr.figures, 'OUT/tr.acceptedNames.tre')
 
-pdf('prairiePhylo_2.2/OUT/dat.tr.figure.pdf')
+pdf('OUT/dat.tr.figure.pdf')
 plot(dat.tr.figures, cex = 0.3)
 dev.off()
 
-pdf('prairiePhylo_2.2/OUT/dat.tr.analysis.pdf')
+pdf('OUT/dat.tr.analysis.pdf')
 plot(dat.tr.analysis, cex = 0.3)
 dev.off()
